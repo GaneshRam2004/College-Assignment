fc6c27871624be021a90630ef8b6f45f10bc5f37
GaneshRam2004 <115285157+GaneshRam2004@users.noreply.github.com>
2022-10-07 22:50:46 +0530

    initial commit

---
+++ Core Java Exercise Book/JavaDocDemo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class JavaDocDemo {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+      int x = reader.nextInt();
+      int y = reader.nextInt();
+      int sum = x + y;
+      System.out.println("Sum is: " + sum);
+  }
+}
